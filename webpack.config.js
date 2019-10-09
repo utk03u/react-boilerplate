a040072fe5479ffa7df37c7d95256566571e0428
Utkarsh Upadhyay <Utkarsh.Upadhyay@skillsoft.com>
2019-10-09 16:32:18 +0530

    Updated the build folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'build'),
     publicPath: '/',
     filename: 'bundle.js',
   },
